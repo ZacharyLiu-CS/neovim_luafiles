87a2774de0c66d6a4428bb03bb35a1d93f279a21
zhenliu <liuzhenm@mail.ustc.edu.cn>
2023-02-14 11:02:12 +0800

    remove flake8

--- lua/plugins/lsp_config/null_ls_config.lua
+++ lua/plugins/lsp_config/null_ls_config.lua
@@ -11,7 +11,6 @@ local sources = {
     null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.write_good,
 }
 
