9bf728e277e589d2bacaece4a6017b420348cfd0
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2024-01-02 17:30:43 +0800

    update the new py module

--- skeletons/skel.py
+++ skeletons/skel.py
@@ -6,10 +6,22 @@
 # Created by @NAME@ on @DATE@.
 # Copyright (c) @YEAR@ @EMAIL@.
 #
+from typing import *
+
+from pprint import pprint
+import heapq
+from collections import Counter
+from collections import deque
+import bisect
+
+MAXINT = 2**63-1
+MININT = -2**63
+
 def main():
     """ Main program """
     # Code goes over here.
     return 0
+
 
 if __name__ == "__main__":
     main()
