19e1af8e872502e20e2be8188d68540f2177ba18
zacharyzliu <zacharyzliu@tencent.com>
2025-07-23 10:38:07 +0800

    macos pbcopy

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,58 +1,71 @@
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  wildmenu = true,                         -- allows you to use <Left> or <Right> to navigate through the completion lists. 
-  wildmode ="longest:list,full",           -- complete to longest string and show the the match list
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  spell = true,                            -- enable the spell check
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 500,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  autoread = true,                         -- auto read while file is modified outside
-  autowrite = true,                        -- auto save content
-  autochdir = true,                        -- auto change the current working directory 
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  colorcolumn = '100',                       -- highlight the column separator
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  list = true,                             -- set eol (end of line), tab and tail space in different way
+  backup = false,                              -- creates a backup file
+  clipboard = "unnamedplus",                   -- allows neovim to access the system clipboard
+  cmdheight = 1,                               -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" },     -- mostly just for cmp
+  conceallevel = 0,                            -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                      -- the encoding written to a file
+  wildmenu = true,                             -- allows you to use <Left> or <Right> to navigate through the completion lists.
+  wildmode = "longest:list,full",              -- complete to longest string and show the the match list
+  hlsearch = true,                             -- highlight all matches on previous search pattern
+  spell = true,                                -- enable the spell check
+  ignorecase = true,                           -- ignore case in search patterns
+  mouse = "a",                                 -- allow the mouse to be used in neovim
+  pumheight = 10,                              -- pop up menu height
+  showmode = false,                            -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                             -- always show tabs
+  smartcase = true,                            -- smart case
+  smartindent = true,                          -- make indenting smarter again
+  splitbelow = true,                           -- force all horizontal splits to go below current window
+  splitright = true,                           -- force all vertical splits to go to the right of current window
+  swapfile = false,                            -- creates a swapfile
+  termguicolors = true,                        -- set term gui colors (most terminals support this)
+  timeoutlen = 500,                            -- time to wait for a mapped sequence to complete (in milliseconds)
+  autoread = true,                             -- auto read while file is modified outside
+  autowrite = true,                            -- auto save content
+  autochdir = true,                            -- auto change the current working directory
+  undofile = true,                             -- enable persistent undo
+  updatetime = 300,                            -- faster completion (4000ms default)
+  writebackup = false,                         -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                            -- convert tabs to spaces
+  shiftwidth = 2,                              -- the number of spaces inserted for each indentation
+  tabstop = 2,                                 -- insert 2 spaces for a tab
+  cursorline = true,                           -- highlight the current line
+  colorcolumn = '100',                         -- highlight the column separator
+  number = true,                               -- set numbered lines
+  relativenumber = true,                       -- set relative numbered lines
+  numberwidth = 4,                             -- set number column width to 2 {default 4}
+  list = true,                                 -- set eol (end of line), tab and tail space in different way
   listchars = "eol:⏎,tab:␉·,trail:␠,nbsp:⎵",
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,                             -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
-  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
-  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "SauceCodePro Nerd Font Mono:h15",  -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+  signcolumn = "yes",                          -- always show the sign column, otherwise it would shift the text each time
+  wrap = true,                                 -- display lines as one long line
+  linebreak = true,                            -- companion to wrap, don't split words
+  scrolloff = 8,                               -- minimal number of screen lines to keep above and below the cursor
+  sidescrolloff = 8,                           -- minimal number of screen columns either side of cursor if wrap is `false`
+  guifont = "SauceCodePro Nerd Font Mono:h15", -- the font used in graphical neovim applications
+  whichwrap = "bs<>[]hl",                      -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
-
+vim.g.clipboard = {
+  name = 'macOS-clipboard',
+  -- When you use the "+y or "*y commands in Neovim, it will copy the selected text to the clipboard using mac pbcopy .
+  copy = {
+    ['+'] = 'mac pbcopy', -- You can use `mac link pbcopy` first, then just use pbcopy
+    ['*'] = 'mac pbcopy',
+  },
+  -- When you use the "+p or "*p commands in Neovim, it will paste the clipboard content by invoking mac pbpaste.
+  paste = {
+    ['+'] = 'mac pbpaste',
+    ['*'] = 'mac pbpaste',
+  },
+  cache_enabled = 0,
+}
 -- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
-vim.opt.shortmess:append "c"                           -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
-vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
-vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
+vim.opt.shortmess:append "c"                          -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append "-"                          -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove({ "c", "r", "o" })       -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- separate vim plugins from neovim in case vim still in use
 vim.loader.enable()
